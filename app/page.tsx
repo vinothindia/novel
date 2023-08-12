03251c69ef34854f6f9cafe2378aaebbff400dcc
vinothindia <vinothindia@gmail.com>
2023-08-12 16:06:51 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Menu from "./menu";
 export default function Page() {
   return (
     <div className="flex min-h-screen flex-col items-center sm:px-5 sm:pt-[calc(20vh)]">
-      <span class="lg:text-[6px] md:text-[5px] sm:text-[4px] sm:pl-[5px] sm:pt-[33px] sm:w-[110px] absolute md:w-[80px] md:pt-[35px] lg:w-[100px] lg:pt-[40px] font-semibold">Upgrade. Upskill. Upsell.</span>
+      <span style="color:darkolivegreen;font-weight:bold">Upgrade. Upskill. Upsell.</span>
       <a
         href="/github"
         target="_blank"
